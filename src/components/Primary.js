ec5cf33b7db8751b5dfc78324bb2c35ac6e7a820
johnnydrach <johnnydrach>
2023-09-08 17:54:36 +0000

    First Commit from Locofy Builder

---
+++ src/components/Primary.js
@@ -0,0 +1,10 @@
+import "./Primary.css";
+const Primary = () => {
+  return (
+    <button className="primary" autoFocus tabIndex={1}>
+      <div className="test-button">Test Button</div>
+    </button>
+  );
+};
+
+export default Primary;
